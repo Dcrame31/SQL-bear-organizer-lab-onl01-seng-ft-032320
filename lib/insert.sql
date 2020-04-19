d4130ff4a78df14a4d7695911de9d8c0b4026008
Diane Crame <dcrame@icloud.com>
2020-04-19 01:22:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,7 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr Chocolate", 6, "female", "brown", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr Chocolate", 6, "male", "brown", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "female", "brown", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "brown", "goofy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "male", "brown", "great", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 19, "female", "black", "happy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "female", "brown", "sleepy", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, "female", "brown", "friendly", true);
